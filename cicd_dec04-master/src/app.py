7c889625e94157e5eaf8565820998d3206745529
Lucius G <lucius.gara@gmail.com>
2025-12-11 20:06:33 -0600

    added mul sin with tests and test for sub, covered

--- cicd_dec04-master/src/app.py
+++ cicd_dec04-master/src/app.py
@@ -1,5 +1,16 @@
+import math
+
 def add (a, b):
     return a+b
 
 def sub (a, b):
     return a-b
+
+#basic
+def mul(a, b):
+    """Return the product of a and b."""
+    return a * b
+#advanced
+def sin(x):
+    """Return the sine of x (x in radians)."""
+    return math.sin(x)
--- cicd_dec04-master/tests/test_app.py
+++ cicd_dec04-master/tests/test_app.py
@@ -1,14 +1,28 @@
 import sys
 from pathlib import Path
-
+import math
+from math import pi
 root = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(root / "src"))
 
 
-from app import add
+from app import add, mul, sin, sub
 
 def test_add():
     assert add(5, 6) == 11
 
+def test_sub():
+    assert sub(5, 2) == 3
+
+
 def test_add2():
     assert add(5, 6) != 10
+
+def test_mul():
+    assert mul(4, 5) == 20
+
+def test_sin_zero():
+    assert sin(0) == 0
+
+def test_sin_pi_over_two():
+    assert sin(pi/2) == 1
